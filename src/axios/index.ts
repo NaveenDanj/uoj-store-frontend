a0d9af15db270098826aa1d5c1cdbd7163ee1c5e
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-17 12:36:09 +0530

    added dep changes 1

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://15.206.79.187/api',
+    baseURL: 'https://uoj.uk.to/api',
     timeout: 6*10000, 
     headers: {
         'Content-Type': 'application/json',
