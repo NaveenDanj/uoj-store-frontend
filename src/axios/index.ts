90e385dc133e0b2239259c445f1f10f01090a360
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-18 02:44:56 +0530

    added new workflow file

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://uoj.uk.to/api',
+    baseURL: 'http://localhost:5001/api',
     timeout: 6*10000, 
     headers: {
         'Content-Type': 'application/json',
