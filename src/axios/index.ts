cf33c2cbfeac332c9de221dd6e82826153541755
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-24 01:59:48 +0530

    added build fixes 3

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://uoj.uk.to/api',
     timeout: 6*10000, 
     headers: {
         'Content-Type': 'application/json',
