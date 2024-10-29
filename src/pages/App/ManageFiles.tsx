25efc60b5eef06d9d28c8d162074e220056c3410
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-29 22:30:02 +0530

    addde latest changes

--- src/pages/App/ManageFiles.tsx
+++ src/pages/App/ManageFiles.tsx
@@ -166,6 +166,7 @@ export default function ManageFilePage() {
                         </TableRow>
                     ))}
                 </TableBody>
+                
             </Table>
 
             {/* Pagination Controls */}
